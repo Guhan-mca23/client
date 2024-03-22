a2cbee0a1f50052d2bc650856f646ed591aae1c4
Guhan-mca23 <139735072+Guhan-mca23@users.noreply.github.com>
2024-03-22 18:39:05 +0530

    submited

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+export default function Home(){
+    return(
+        <>
+        <div id="home">
+         <h3> Welcome   <span id="span-1"> our bank </span></h3>
+         <h4>We also have an online banking</h4>
+         <p> system available 24/7 from any device with an internet connection </p>
+          <p>Create a new account,balance checks and deposit your money</p>
+
+        </div>
+        
+       
+        </>
+    )
+}
